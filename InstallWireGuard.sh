817b6ec4680a3b2023366b65b9fc4e2097c914c0
therodfather <34672275+therodfather@users.noreply.github.com>
2019-09-18 16:07:31 +0800

    Update InstallWireGuard.sh

--- InstallWireGuard.sh
+++ InstallWireGuard.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #Update
 sudo apt-get update && sudo apt-get upgrade -y
 
@@ -12,17 +14,35 @@ unzip master.zip
 cd algo-master/
 
 #Install Algo
-apt-get update && apt-get install \
-    build-essential \
-    libssl-dev \
-    libffi-dev \
-    python-dev \
-    python-pip \
-    python-setuptools \
-    python-virtualenv -y
+apt-get update && apt-get install build-essential libssl-dev libffi-dev python-dev python-pip python-setuptools python-virtualenv -y
 
 #Start Virtual Environment
-python -m virtualenv --python=`which python2` env &&
-    source env/bin/activate &&
-    python -m pip install -U pip virtualenv &&
-    python -m pip install -r requirements.txt
+python -m virtualenv --python=`which python2` env && source env/bin/activate && python -m pip install -U pip virtualenv && python -m pip install -r requirements.txt
+
+#Create INI file
+touch vultr.ini
+echo "[default] 
+key = EUAVYIMY6GUSK4OS5WUROW2MIB7XD3OHDADA" > vultr.ini
+#Run Algo
+./algo << EOF
+4
+MyWire
+N
+N
+N
+y
+N
+/root/algo-master/vultr.ini
+14
+EOF
+
+cd configs/
+newip=$(ls -d */)
+cd $newip/wireguard/
+
+#Install grencode
+sudo apt install qrencode -y
+qrencode -t ansiutf8 < phone.conf
+
+
+
