a0d43c3bf2b5cacf48d5d2e2eb7f5f29e77228d5
therodfather <34672275+therodfather@users.noreply.github.com>
2019-09-29 10:37:43 +0800

    Update InstallLifeRay.sh

--- InstallLifeRay.sh
+++ InstallLifeRay.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 #Update
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y
 
 #Install Java
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo add-apt-repository ppa:openjdk-r/ppa 
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 
 #Download LifeRay
 wget https://github.com/liferay/liferay-portal/releases/download/7.2.0-ga1/liferay-ce-portal-tomcat-7.2.0-ga1-20190531153709761.tar.gz
 
-#
+#Unpack
+tar -xvzf liferay-ce-portal-tomcat-7.2.0-ga1-20190531153709761.tar.gz
+
+#Execute program
+cd liferay-portal-7.2.0-ga1/tomcat-9.0.17/bin/
+./startup.sh
