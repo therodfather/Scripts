2765189c9f8bc36ef27049dbce8d8497b2148f2a
therodfather <34672275+therodfather@users.noreply.github.com>
2019-11-08 12:12:54 +0800

    Create PuppetAgent.sh

---
+++ PuppetAgent.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Update
+apt-get update
+
+#Download Puppet repo
+wget https://apt.puppetlabs.com/puppet6-release-xenial.deb
+
+#Unpack
+dpkg -i puppet6-release-xenial.deb
+
+#Update
+apt-get update
+
+#Install the Agent
+apt-get install puppet-agent -y
+
+#Start agent
+systemctl start puppet
+systemctl status puppet
