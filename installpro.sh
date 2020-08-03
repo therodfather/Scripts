69e4e173d046b97af85bb48dbf791cf7e1d15a4f
therodfather <34672275+therodfather@users.noreply.github.com>
2020-08-03 13:12:19 -0400

    Create installpro.sh

---
+++ installpro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/prometheus/prometheus/releases/download/v2.20.0/prometheus-2.20.0.linux-amd64.tar.gz
+
+tar -xzf prometheus-2.20.0.linux-amd64.tar.gz
+
+cd prometheus-2.20.0.linux-amd64
+
+./prometheus
