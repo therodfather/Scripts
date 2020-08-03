ae64d0636fd9a7c0917b0e02b438af71119f010f
therodfather <34672275+therodfather@users.noreply.github.com>
2020-08-03 15:33:23 -0400

    Update installpro.sh

--- installpro.sh
+++ installpro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt update && apt upgrade -y
+
 wget https://github.com/prometheus/prometheus/releases/download/v2.20.0/prometheus-2.20.0.linux-amd64.tar.gz
 
 tar -xzf prometheus-2.20.0.linux-amd64.tar.gz
